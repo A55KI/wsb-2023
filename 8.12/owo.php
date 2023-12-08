df49b37c973ae132245c41eeed0d0affd39c79c3
TLK 310 Roztocze <147035964+A55KI@users.noreply.github.com>
2023-12-08 11:15:55 +0100

    8.12
    SIECI ROBIMY GRINDERA

---
+++ 8.12/owo.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>NIE</title>
+</head>
+<body>
+    <h1>ŻEGNAJ</h1>
+
+<?php
+    print "ZABIJ SIĘ".$_REQUEST["UwU"]." ".$_REQUEST["OwO"]."<br>";
+?></body>
+</html>
